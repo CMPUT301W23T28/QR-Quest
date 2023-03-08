42812daba94ef7cefaf19f8d9f4ea9e3db52a6d6
Adnan <nadeer@ualberta.ca>
2023-03-08 00:31:01 -0600

    refactoring primarily

--- QR_Quest/app/src/main/java/com/example/qr_quest/QRFragment.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/QRFragment.java
@@ -2,7 +2,6 @@ package com.example.qr_quest;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,7 +10,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -39,7 +37,7 @@ public class QRFragment extends DialogFragment {
         yesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), AddPhoto.class);
+                Intent intent = new Intent(getActivity(), AddPhotoActivity.class);
                 startActivity(intent);
             }
         });
