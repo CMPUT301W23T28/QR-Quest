688c8f96e0c7c016f26dbb1c365ca2c099a97887
Harjot Singh <harjot1399@gmail.com>
2023-03-12 15:15:02 -0600

    abc

--- QR_Quest/app/src/main/java/com/example/qr_quest/QRActivity.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/QRActivity.java
@@ -1,21 +1,30 @@
 package com.example.qr_quest;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QRActivity extends AppCompatActivity {
     ImageButton backButton;
+    ImageView showImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qractivity);
         backButton=findViewById(R.id.back);
+        showImage = findViewById(R.id.image_shown);
 
+//        String imageString =
+//        setImageFromBase64(base64String, imageView);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +34,11 @@ public class QRActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void setImageFromBase64(String base64String, ImageView imageView) {
+        byte[] decodedString = Base64.decode(base64String, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        imageView.setImageBitmap(decodedByte);
     }
 }
