2c96b73e275c66ad373b721f5eef0dfaaaf0a37d
mhshadab <shadab@ualberta.ca>
2023-03-13 00:50:42 -0600

    Added java docs to walletAdapter, QRActivity and Wallet

--- QR_Quest/app/src/main/java/com/example/qr_quest/QRActivity.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/QRActivity.java
@@ -11,8 +11,19 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * QRActivity displays the scanned QR code image and a back button.
+ */
+
 public class QRActivity extends AppCompatActivity {
 
+
+    /**
+     * This method is called when the activity is created.
+     * It sets up the view and displays the image of the scanned QR code.
+     *
+     * @param savedInstanceState The saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,14 @@ public class QRActivity extends AppCompatActivity {
         }
 
         backButton.setOnClickListener(new View.OnClickListener() {
+
+            /**
+
+             * This method is called when the user clicks a button. It creates a new Intent and starts the HomeActivity,
+             * adding data to the Intent to indicate that the user is coming from the QRActivity. Once the Intent is started,
+             * the QRActivity is finished and removed from the activity stack.
+             * @param view The view that was clicked.
+             */
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(QRActivity.this, HomeActivity.class);
@@ -36,6 +55,14 @@ public class QRActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    /**
+     * This method decodes a Base64 encoded string and sets the resulting image to an ImageView.
+     *
+     * @param base64String The Base64 encoded string of the image.
+     * @param imageView The ImageView to display the decoded image.
+     */
 
     public void setImageFromBase64(String base64String, ImageView imageView) {
         byte[] decodedString = Base64.decode(base64String, Base64.DEFAULT);
--- QR_Quest/app/src/main/java/com/example/qr_quest/Wallet.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/Wallet.java
@@ -1,36 +1,78 @@
 package com.example.qr_quest;
 
+/**
+ * The Wallet class represents a wallet item containing a string identifier d, name and points.
+ */
 public class Wallet {
     String d;
     String name;
     String points;
 
+    /**
+     * Creates a new Wallet object with the specified identifier, name, and points.
+     *
+     * @param d the identifier for the wallet item
+     * @param name the name of the wallet item
+     * @param points the number of points associated with the wallet item
+     */
     public Wallet(String d, String name, String points) {
         this.d = d;
         this.name = name;
         this.points = points;
     }
 
+    /**
+     * Returns the identifier of the wallet item.
+     *
+     * @return the identifier of the wallet item
+     */
     public String getD() {
         return d;
     }
 
+
+    /**
+     * Sets the identifier of the wallet item.
+     *
+     * @param d the new identifier for the wallet item
+     */
     public void setD(String d) {
         this.d = d;
     }
 
+
+    /**
+     * Returns the name of the wallet item.
+     *
+     * @return the name of the wallet item
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the wallet item.
+     *
+     * @param name the new name for the wallet item
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the number of points associated with the wallet item.
+     *
+     * @return the number of points associated with the wallet item
+     */
     public String getPoints() {
         return points;
     }
 
+    /**
+     * Sets the number of points associated with the wallet item.
+     *
+     * @param points the new number of points for the wallet item
+     */
     public void setPoints(String points) {
         this.points = points;
     }
