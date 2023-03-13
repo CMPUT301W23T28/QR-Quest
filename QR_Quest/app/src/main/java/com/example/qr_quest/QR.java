69d1ccf623d1e65ac43b193d796add20d59aa645
mhshadab <shadab@ualberta.ca>
2023-03-13 03:38:25 -0600

    Added java docs to QR and User

--- QR_Quest/app/src/main/java/com/example/qr_quest/QR.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/QR.java
@@ -5,6 +5,11 @@ import com.google.common.hash.Hashing;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * QR class represents a QR code and stores its content, hash value, score, and avatar.
+ * It also provides methods to generate hash value, score the QR code, and create an avatar from its hash value.
+ * Additionally, it provides methods to set and get location, caption, and image string of the QR code.
+ */
 public class QR implements Serializable {
     private String content, hashValue, name, icon;
     private Avatar avatar;
@@ -16,6 +21,12 @@ public class QR implements Serializable {
 
     QR () {}
 
+    /**
+     * Constructs a new QR object with the given content.
+     * It generates the hash value, scores the QR code, and creates an avatar from the hash value.
+     *
+     * @param content the content of the QR code
+     */
     public QR(String content){
         this.content = content;
         this.score = 0;
@@ -24,12 +35,25 @@ public class QR implements Serializable {
         createAvatar(hashValue);
     }
 
+    /**
+     * Generates the hash value of the given string using SHA-256 algorithm and UTF-8 encoding.
+     *
+     * @param string the string to be hashed
+     */
+
     private void generateHashValue(String string) {
         this.hashValue = Hashing.sha256()
                 .hashString(string, StandardCharsets.UTF_8)
                 .toString();
     }
 
+    /**
+     * Scores the QR code based on its hash value.
+     * The score is calculated by XOR-ing the decimal value of each character with the count of consecutive same characters,
+     * raised to the power of the count minus one.
+     *
+     * @param qrHashValue the hash value of the QR code
+     */
     private void scoreQR(String qrHashValue) {
         int count = 0;
         char previousChar = ' ';
@@ -51,41 +75,106 @@ public class QR implements Serializable {
         }
     }
 
+    /**
+     * Creates an avatar from the hash value of the QR code.
+     *
+     * @param qrHashValue the hash value of the QR code
+     */
+
     private void createAvatar(String qrHashValue) {
         this.avatar = new Avatar(qrHashValue);
         this.name = this.avatar.getAvatarName();
         this.icon = this.avatar.getAvatarFigure();
     }
 
+
+    /**
+     * Returns the score of the QR code.
+     * The score is calculated by XOR-ing the decimal value of each character with the count of consecutive same characters,
+     * raised to the power of the count minus one.
+     * @return the score of the QR code
+     */
     public int getScore() {
         return this.score;
     }
+
+    /**
+     * Returns the hash value of the QR code.
+     * @return the hash value of the QR code
+     */
 
     public String getHashValue() {
         return this.hashValue;
     }
 
+    /**
+     * Returns the avatar object of the QR code.
+     * @return the avatar object of the QR code
+     */
+
     public Avatar getAvatar() {
         return this.avatar;
     }
+
+    /**
+     * Returns the name of the QR code's avatar.
+     * @return the name of the QR code's avatar
+     */
 
     public String getQRName(){
         return this.name;
     }
 
+    /**
+     * Returns the icon of the QR code's avatar.
+     * @return the icon of the QR code's avatar
+     */
+
     public String getQRIcon(){
         return this.icon;
     }
+    /**
+     * Returns the latitude of the QR code's location.
+     * @return the latitude of the QR code's location
+     */
+
 
     public double getLatitude() { return this.latitude;}
 
+    /**
+     * Returns the longitude of the QR code's location.
+     * @return the longitude of the QR code's location
+     */
+
     public double getLongitude() { return this.longitude;}
+
+    /**
+     * Returns the city of the QR code's location.
+     * @return the city of the QR code's location
+     */
 
     public String getCity() { return this.city;}
 
+    /**
+     * Returns the caption of the QR code's image.
+     * @return the caption of the QR code's image
+     */
+
     public String getCaption() { return this.caption;}
 
+    /**
+     * Returns the image string of the QR code's image.
+     * @return the image string of the QR code's image
+     */
+
     public String getImgString() { return this.imgString;}
+
+    /**
+     * Sets the location of the QR code.
+     * @param latitude the latitude of the QR code's location
+     * @param longitude the longitude of the QR code's location
+     * @param city the city of the QR code's location
+     */
 
     public void setLocation(double latitude, double longitude, String city) {
         this.latitude = latitude;
@@ -93,7 +182,16 @@ public class QR implements Serializable {
         this.city = city;
     }
 
+    /**
+     * Sets the caption of the QR code's image.
+     * @param caption the caption of the QR code's image
+     */
+
     public void setCaption(String caption) { this.caption = caption;}
 
+    /**
+     * Sets the image string for the object.
+     * @param imgString a string representation of the image to be set
+     */
     public void setImgString(String imgString) { this.imgString = imgString;}
  }
--- QR_Quest/app/src/main/java/com/example/qr_quest/User.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/User.java
@@ -12,6 +12,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The User class represents a user in the QR Quest game.
+ * It contains information such as the user's username, email, first and last name, phone number, score and QR codes.
+ */
 public class User {
 
     private String username;
@@ -24,29 +28,110 @@ public class User {
     private List<String> qrCodes;
 
     // Getters and setters for all fields
+
+    /**
+     * Returns the user's username.
+     * @return The user's username.
+     */
     public String getUsername() { return username; }
+
+    /**
+     * Sets the user's username.
+     * @param username The new username.
+     */
     public void setUsername(String username) { this.username = username; }
 
+    /**
+     * Returns the user's score.
+     * @return The user's score.
+     */
     public long getScore() { return score; }
+
+    /**
+     * Sets the user's score.
+     * @param score The new score.
+     */
     public void setScore(int score) { this.score = score; }
 
+    /**
+     * Returns a list of the user's QR codes.
+     * @return A list of the user's QR codes.
+     */
+
     public List<String> getQRCodes() { return qrCodes; }
+
+
+    /**
+     * Sets the user's QR codes.
+     * @param qrCodes A list of the user's QR codes.
+     */
+
     public void setQRCodes(List<String> qrCodes) { this.qrCodes = qrCodes; }
 
+    /**
+     * Returns the user's email address.
+     * @return The user's email address.
+     */
     public String getEmail() { return email; }
+
+    /**
+     * Sets the user's email address.
+     * @param email The new email address.
+     */
     public void setEmail(String email) { this.email = email; }
 
+    /**
+     * Returns the user's first name.
+     * @return The user's first name.
+     */
+
     public String getFirstName() { return firstName; }
+
+    /**
+     * Sets the user's first name.
+     * @param firstName The new first name.
+     */
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
+    /**
+     * Returns the user's last name.
+     * @return The user's last name.
+     */
+
     public String getLastName() { return lastName; }
+
+    /**
+     * Sets the user's last name.
+     * @param lastName The new last name.
+     */
     public void setLastName(String lastName) { this.lastName = lastName; }
 
+
+
+    /**
+     * Returns the user's phone number.
+     * @return The user's phone
+     */
     public String getPhoneNumber() { return phoneNumber; }
+
+    /**
+     * Sets the user's phone number.
+     * @param phoneNumber The new  phone number.
+     */
     public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
     public User() {}
 
+    /**
+     * Constructor for the User class.
+     * @param username The user's username.
+     * @param email The user's email address.
+     * @param firstName The user's first name.
+     * @param lastName The user's last name.
+     * @param phoneNumber The user's phone number.
+     * @param score The user's score.
+     * @param qrCodes A list of the user's QR codes.
+     */
     public User(String username, String email, String firstName, String lastName, String phoneNumber,
                 long score, ArrayList<String> qrCodes) {
         this.username = username;
