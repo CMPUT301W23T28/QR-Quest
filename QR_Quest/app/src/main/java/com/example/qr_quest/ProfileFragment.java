d819fa8e11190afcd411047f3474f67172154a97
Adnan <nadeer@ualberta.ca>
2023-03-28 15:19:20 -0500

    fixed highest lowest qr

--- QR_Quest/app/src/main/java/com/example/qr_quest/ProfileFragment.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/ProfileFragment.java
@@ -32,6 +32,8 @@ public class ProfileFragment extends Fragment implements ItemClickListener{
     private WalletAdapter adapter;
     private String prevUserName;
 
+    private QR highestQR, lowestQR;
+
     private androidx.cardview.widget.CardView highest_Card, lowest_Card;
 
     public ProfileFragment() {}
@@ -83,27 +85,29 @@ public class ProfileFragment extends Fragment implements ItemClickListener{
                         statsTextView.setText(userDoc.getLong("score") + "pts       " +
                                 qr_num + " QR's Collected       Rank: " + rank));
 
-                QRDatabase.getHighestQR(username, qrDoc -> {
-                    if (qrDoc != null) {
+                QRDatabase.getHighestQR(username, qrCode -> {
+                    if (qrCode != null) {
+                        highestQR = qrCode;
                         TextView highestIcon = view.findViewById(R.id.highest_icon);
                         TextView highestName = view.findViewById(R.id.highest_name);
                         TextView highestPoint = view.findViewById(R.id.highest_points);
 
-                        highestIcon.setText(qrDoc.getString("avatar"));
-                        highestName.setText(qrDoc.getId());
-                        highestPoint.setText("Highest QR: " + qrDoc.getLong("score") + " pts");
+                        highestIcon.setText(qrCode.getQRIcon());
+                        highestName.setText(qrCode.getQRName());
+                        highestPoint.setText("Highest QR: " + qrCode.getScore() + " pts");
                     }
                 });
 
-                QRDatabase.getLowestQR(username, qrDoc -> {
-                    if (qrDoc != null) {
+                QRDatabase.getLowestQR(username, qrCode -> {
+                    if (qrCode != null) {
+                        lowestQR = qrCode;
                         TextView lowestIcon = view.findViewById(R.id.lowest_icon);
                         TextView lowestName = view.findViewById(R.id.lowest_name);
                         TextView lowestPoint = view.findViewById(R.id.lowest_points);
 
-                        lowestIcon.setText(qrDoc.getString("avatar"));
-                        lowestName.setText(qrDoc.getId());
-                        lowestPoint.setText("Lowest QR: " + qrDoc.getLong("score") + " pts");
+                        lowestIcon.setText(qrCode.getQRIcon());
+                        lowestName.setText(qrCode.getQRName());
+                        lowestPoint.setText("Lowest QR: " + qrCode.getScore() + " pts");
                     }
                 });
 
