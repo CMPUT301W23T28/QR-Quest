a5363841a8c83ac2ee15aff3eae1f52910451920
Adnan <nadeer@ualberta.ca>
2023-03-31 19:10:25 -0500

    adding comments

--- QR_Quest/app/src/main/java/com/example/qr_quest/QRActivity.java
+++ QR_Quest/app/src/main/java/com/example/qr_quest/QRActivity.java
@@ -31,6 +31,7 @@ public class QRActivity extends AppCompatActivity {
 
     private EditText commentEditText;
     private Button addCommentButton;
+    private CommentAdapter commentAdapter;
 
     /**
      * This method is called when the activity is created.
@@ -110,47 +111,38 @@ public class QRActivity extends AppCompatActivity {
                 // Call fillComment to retrieve all the comments for the scanned QR code
                 Comment.fillComment(scannedQR, comments -> {
                     RecyclerView recyclerView = view1.findViewById(R.id.recyclerView);
-                    CommentAdapter adapter = new CommentAdapter(comments);
+                    commentAdapter = new CommentAdapter(comments);
                     recyclerView.setHasFixedSize(true);
                     recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                    recyclerView.setAdapter(adapter);
+                    recyclerView.setAdapter(commentAdapter);
 
                     final AlertDialog alertDialog = new AlertDialog.Builder(QRActivity.this)
                             .setView(view1)
                             .create();
                     alertDialog.show();
 
-                    commentEditText = findViewById(R.id.commentEditText);
-                    addCommentButton = findViewById(R.id.commentBtn);
+                    commentEditText = view1.findViewById(R.id.commentEditText);
+                    addCommentButton = view1.findViewById(R.id.commentBtn);
 
                     addCommentButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             String commentText = commentEditText.getText().toString();
                             if (!commentText.isEmpty()) {
-
                                 checkUserName(user, check -> QRDatabase.addComment(commentText, check, scannedQR, success -> {
                                     if (success) {
                                         Toast.makeText(QRActivity.this, "Comment Added", Toast.LENGTH_SHORT).show();
                                         Comment newComment = new Comment(check, commentText);
                                         comments.add(newComment);
+                                        commentAdapter.notifyDataSetChanged();
+                                        commentEditText.setText("");
                                     }
                                 }));
-
-
-
-                                // Add the new comment to the list of comments
-                                commentList.add(newComment);
-
-                                // Update the RecyclerView to display the updated list of comments
-                                CommentAdapter.notifyDataSetChanged();
-
-                                // Clear the text in the EditText view
-                                commentEditText.setText("");
+                            } else {
+                                Toast.makeText(QRActivity.this, "Your comment was empty!", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-
                 });
             }
         });
